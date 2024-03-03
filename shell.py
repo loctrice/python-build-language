585bdd567a9405db044ccd57a25eeb7c94b662b0
Steven Griffith <stevengriffith@blackbetty.lan>
2024-03-03 15:49:03 -0500

    Lexer

---
+++ shell.py
@@ -0,0 +1,12 @@
+
+import basic
+
+while True:
+    text = input('basic > ')
+    result, error = basic.run('<stdin>', text)
+
+    if error:
+        print('error')
+        print(error.as_string())
+    else:
+        print(result)
